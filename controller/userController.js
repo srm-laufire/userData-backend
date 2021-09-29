22c3b87d9a9d4f6e8226b80568121b6a69cbc8af
srm-laufire <srm@laufire.in>
2021-09-29 12:18:47 +0000

    Introduced removeUser.

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,5 @@
 const user = require('../models').Users;
+const { Op } = require('sequelize');
 
 exports.createUser = async (req, res) => {
     const { name, age, gender } = req.body;
@@ -14,5 +15,18 @@ exports.createUser = async (req, res) => {
 
 exports.allUsers = async (req, res) => {
     const users = await user.findAll({})
+    
     res.json(users);
 };
+
+exports.removeUser = async (req, res) => {
+    const result = await user.destroy({
+        where: {
+            id: {
+                [Op.eq]: req.params.id,
+            }
+        }
+    });
+
+    res.json(result);
+}
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../controller/userController");
 
 router.route('/').post(userController.createUser).get(userController.allUsers);
+router.route('/:id').delete(userController.removeUser);
 
 module.exports = router;
